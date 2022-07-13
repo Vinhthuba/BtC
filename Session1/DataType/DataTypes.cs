c8a91dc42874e9a06a5a9d5425ab4661323e844e
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-07-13 16:27:33 +0700

    Lap1

---
+++ Session1/DataType/DataTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Example
+{
+    static void Main(string[] args)
+    {
+        int intVal = 10;
+        double dblVal = 3.142;
+        string strVal = "Fpt Aptech";
+        Console.WriteLine("{0} is an integer value", intVal);
+        Console.WriteLine("{0} is an double value", dblVal);
+        Console.WriteLine("{0} is an string", strVal);
+        Console.ReadLine();
+    }
+}
