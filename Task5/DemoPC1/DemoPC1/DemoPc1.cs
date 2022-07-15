ab31f802f09d187570c071e8ad793b95d8c2f03e
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-07-15 16:47:48 +0700

    Task5

---
+++ Task5/DemoPC1/DemoPC1/DemoPc1.cs
@@ -0,0 +1,17 @@
+﻿class ex1
+{
+    static void Main(string[] args)
+    {
+        int k = 10;
+        int m = 20;
+        int s = k + m;
+        bool t = m > k;
+        double z = Math.Sqrt(5);
+        Console.WriteLine("The sum is: "+ s);
+        Console.WriteLine("Ii m > k ?"+t);
+        Console.WriteLine("The sqet of 5: " +z);
+        Console.WriteLine("The max value of Int32: " + Int32.MaxValue);
+
+        Console.ReadLine();
+    }
+}
