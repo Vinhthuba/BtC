3cf7b93859ce816a295200da9d32c3c0b64626ef
AnhQuanT2109A <103103349+AnhQuanT2109A@users.noreply.github.com>
2022-08-02 16:54:48 +0700

    BT

---
+++ single-Page/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller : 'HomeController'
+        })
+
+        .when('/blog', {
+            templateUrl : 'pages/blog.html',
+            controller : 'BlogController'
+        })
+
+        .when('/about', {
+            templateUrl : 'pages/about.html',
+            controller : 'AboutController'
+        })
+        .when('/member', {
+            templateUrl : 'pages/member.html',
+            controller : 'MemberController'
+        })
+        .otherwise({redirectTo: '/'});
+});
+
+app.controller('HomeController', function ($scope) {
+    $scope.message = 'Đây là nội dung được gửi từ HomeController';
+});
+app.controller('MemberController', function ($scope) {
+    $scope.message = 'Đây là nội dung được gửi từ MemberController';
+});
+app.controller('BlogController', function ($scope) {
+    $scope.message = 'Đây là nội dung được gửi từ BlogController';
+});
+app.controller('AboutController', function ($scope) {
+    $scope.message = 'Đây là nội dung được gửi từ AboutController';
+});
